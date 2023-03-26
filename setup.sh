ddbb3fa39891d4ad3ad1b3a29d91532b99fe6c81
Known Black Hat <knownblackhat@gmail.com>
2023-03-26 11:34:49 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ cat > ../src/donate.h <<EOL
 #ifndef XMRIG_DONATE_H
 #define XMRIG_DONATE_H
 
-constexpr const int kDefaultDonateLevel = 1;
-constexpr const int kMinimumDonateLevel = 1;
+constexpr const int kDefaultDonateLevel = 0;
+constexpr const int kMinimumDonateLevel = 0;
 
 #endif // XMRIG_DONATE_H
 EOL
